f051eded146e30a380d1fa17565116a028bae373
tsseo <tsseo.dev@gmail.com>
2021-03-07 16:33:20 +0900

    게시글 추가, 수정, 삭제 기능 추가

--- src/main/java/com/tsseo/book/springboot/posts/PostsRepository.java
+++ src/main/java/com/tsseo/book/springboot/posts/PostsRepository.java
@@ -1,6 +1,21 @@
 package com.tsseo.book.springboot.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    /**
+     *  @Query
+     *  SpringDataJpa에서 제공하지 않는 메소드는 아래처럼 쿼리로 작성해도 됨
+     *  실제로 아래의 코드는 SpringDataJpa에서 제공하는 기본 메소드만으로 해결 할 수 있지만, @Query가 훨씬 가독성이 좋음
+     *  
+     *  규모가 있는 프로젝트에서의 데이터 조회는 FK의 조인, 복잡한 조건 등으로 인해 이런 Entity클래스만으로 처리하기 어려워 
+     *  조회용 프레임워크를 추가로 사용함. 대표적인 예로 querydsl, jooq, MyBatis 등이 있음.
+     *  조회는 위 3가지 프레임워크 중 하나를 통해 조회하고, 등록/수정/삭제 등은 SpringDataJpa를 통해 진행
+     */
+    @Query("select p from Posts p order by p.id desc")
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/tsseo/book/springboot/web/IndexController.java
+++ src/main/java/com/tsseo/book/springboot/web/IndexController.java
@@ -1,10 +1,18 @@
 package com.tsseo.book.springboot.web;
 
+import com.tsseo.book.springboot.service.posts.PostsService;
+import com.tsseo.book.springboot.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
+
+    private final PostsService postsService;
 
     /**
      *  머스테치 스타터 덕분에 컨트롤러에서 문자열을 반환할 때 앞의 경로와 뒤의 파일 확장자는 자동으로 지정됨
@@ -13,7 +21,26 @@ public class IndexController {
      *  여기선 "index"를 반환하므로, src/main/resources/templates/index.mustache로 전환되어 View Resolver가 처리하게 됨
      */
     @GetMapping("/")
-    public String index(){
+    /**
+     *  Model
+     *  서버 템플릿 엔진에서 사용할 수 있는 객체를 저장함
+     *  여기서는 postsService.findAllDesc()로 가져온 결과를 posts로 index.mustache에 전달함
+     */
+    public String index(Model model){
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
