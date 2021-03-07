b0794b760161fd7f3a3849c5d40e3fcd7f32781a
tsseo <tsseo.dev@gmail.com>
2021-03-07 21:31:13 +0900

    스프링 시큐리티 관련

---
+++ src/main/java/com/tsseo/book/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,21 @@
+package com.tsseo.book.springboot.config.auth.dto;
+
+import com.tsseo.book.springboot.domain.user.User;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+
+    private String name;
+    private String email;
+    private String picture;
+
+    // SessionUser에는 인증된 사용자 정보만 필요함. 그 외 정보들은 피료없으므로 name, email, picture만 필드로 선언
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
