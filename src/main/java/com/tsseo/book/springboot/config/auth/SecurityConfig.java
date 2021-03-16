990cae4b987877efa3033aed8eba98ffff8a8e56
tsseo <tsseo.dev@gmail.com>
2021-03-16 21:04:06 +0900

    /profile이 인증 없이도 호출될 수 있게 "/profile" 추가

--- src/main/java/com/tsseo/book/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/tsseo/book/springboot/config/auth/SecurityConfig.java
@@ -26,7 +26,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     // URL 별 권한 관리를 설정하는 옵션의 시작점
                     // authorizeRequests가 선언되어야만 antMatchers 옵션을 사용할 수 있음
                     .authorizeRequests()
-                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                                                                                                    // /profile이 인증 없이도 호출될 수 있게 제외 코드 추가
+                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
                     // 권한 관리 대상을 지정하는 옵션
                     // URL, HTTP 메소드별로 관리가 가능
                     // "/" 등 지정된 URL들은 permitAll() 옵션을 통해 전체 열람 권한을 줌
